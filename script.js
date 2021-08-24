da0cc62d0fbf29611232b2bc0f2619b263059f96
Glitch (glitch-blank-website) <none>
2021-08-24 07:52:59 +0000

    🏮📈 Checkpoint ./style.css:35216239/263 ./index.html:35216239/13 ./script.js:35216239/1594

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 /* js for images sliding-in as you scroll */
 
-$(window).scroll(function(){
-  
-  var wScroll = $(this).scrollTop();
-  
-if(wScroll > $('.homeimages').offset().top - $(window).height()) {
-    
-    var offset = wScroll - $('.homeimages').offset().top + $(window).height()
-    
-    $('.homeimages').css({'transform': 'translate(-' + offset + 'px, 20px);'});
-  }
-});
+// $(window).scroll(function(){
 
+//   var wScroll = $(this).scrollTop();
 
+// if(wScroll > $('.homeimages').offset().top - $(window).height()) {
+
+//     var offset = wScroll - $('.homeimages').offset().top + $(window).height()
+
+//     $('.homeimages').css({'transform': 'translate(-' + offset + 'px, 20px);'});
+//   }
+// });
+
+const options = {};
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isInterescting)
+  })
+},
+options);
